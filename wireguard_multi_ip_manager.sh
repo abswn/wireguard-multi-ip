05afe70f2e26e0527853b37319cf61ea96878db4
abswn <17753757+abswn@users.noreply.github.com>
2025-07-03 17:38:49 +0530

    fix: correct save path for client files

--- wireguard_multi_ip_manager.sh
+++ wireguard_multi_ip_manager.sh
@@ -336,14 +336,14 @@ EOF
     echo "📱 Scan this QR code with the WireGuard mobile app:"
     qrencode -t ansiutf8 < "${CLIENT_DIR}/${client_name}/${client_name}.conf"
     echo "--------------------------------------------------"
-    echo "Or find the config file and QR code at: ${CLIENT_DIR}/${client_name}/${client_name}.conf"
+    echo "Or find the config file and QR code at: ${CLIENT_DIR}/${client_name}/ directory"
 
     # Save a copy of the config and QR code to user's home
     LOGIN_USER=$(logname)
-    DEST_DIR=$(eval echo "~$(logname)/wireguard_clients/${client_name}")
+    DEST_DIR=$(eval echo "~$(logname)/wireguard_clients/")
     mkdir -p "$DEST_DIR"
-    cp -r "${CLIENT_DIR}/${client_name}/" "$DEST_DIR"
-    qrencode -o "${DEST_DIR}/${client_name}_qrcode.png" < "${CLIENT_DIR}/${client_name}/${client_name}.conf"
+    cp -r "${CLIENT_DIR}/${client_name}" "$DEST_DIR"
+    qrencode -o "${DEST_DIR}/${client_name}/${client_name}_qrcode.png" < "${CLIENT_DIR}/${client_name}/${client_name}.conf"
     chown -R "${LOGIN_USER}:${LOGIN_USER}" "$DEST_DIR"
 }
 
